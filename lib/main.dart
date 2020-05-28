6cab1c3e856366fb5d14294b03813f4445c0a081
Jake <jake.k.hassel@gmail.com>
2020-05-27 21:46:56 -0400

    Redesign

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'Theme.dart';
 import 'Task.dart';
 import 'Tasks.dart';
 import 'AddTaskScreen.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 void main() {
   runApp(
@@ -38,69 +39,77 @@ class HomeScreen extends StatelessWidget {
 
     return Container(
       color: Colors.white,
-      child: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.white,
-          body: Stack(
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(top: 40, bottom: 10),
-                    padding: EdgeInsets.symmetric(horizontal: 30),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(date),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Text(
-                              'To-Do List',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline4
-                                  .copyWith(fontWeight: FontWeight.w900),
-                            ),
-                            Icon(Icons.search, color: secondaryColor)
-                          ],
-                        ),
-                      ],
-                    ),
+      child: Scaffold(
+        backgroundColor: primaryColor,
+        body: Stack(
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 40, bottom: 10),
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(date),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            'To-Do List',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline4
+                                .copyWith(fontWeight: FontWeight.w900),
+                          ),
+                          Icon(Icons.search, color: secondaryColor)
+                        ],
+                      ),
+                    ],
                   ),
-                  Expanded(
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: new BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: new BorderRadius.only(
+                        topLeft: const Radius.circular(30.0),
+                        topRight: const Radius.circular(30.0),
+                      ),
+                    ),
+                    // color: primaryColor,
                     child: Consumer<Tasks>(
                       builder: (context, tasks, child) => TaskList(
                         tasks: tasks.all,
                       ),
                     ),
                   ),
-                ],
-              ),
-              Positioned(
-                bottom: 10,
-                width: size.width,
-                child: Center(
-                  child: FloatingActionButton(
-                    backgroundColor: primaryColor,
-                    elevation: 2.0,
-                    tooltip: 'Add To-Do',
-                    child: Icon(Icons.add),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        CupertinoPageRoute(
-                          builder: (context) => AddTaskScreen(),
-                          fullscreenDialog: true,
-                        ),
-                      );
-                    },
-                  ),
                 ),
-              )
-            ],
-          ),
+              ],
+            ),
+            Positioned(
+              bottom: 40,
+              width: size.width,
+              child: Center(
+                child: FloatingActionButton(
+                  backgroundColor: primaryColor,
+                  elevation: 2.0,
+                  tooltip: 'Add To-Do',
+                  child: Icon(Icons.add),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                        builder: (context) => AddTaskScreen(),
+                        fullscreenDialog: true,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
@@ -114,10 +123,23 @@ class TaskList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int numTasks = tasks.length;
     return Container(
+      padding: EdgeInsets.only(top: 10.0),
       child: ListView(
         padding: EdgeInsets.symmetric(horizontal: 10),
         children: <Widget>[
+          if (numTasks > 0)
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                '$numTasks Tasks Remaining',
+                style: Theme.of(context)
+                    .textTheme
+                    .caption
+                    .copyWith(fontWeight: FontWeight.w900),
+              ),
+            ),
           ...this.tasks.map((task) {
             return TaskListItem(task: task);
           })
@@ -134,24 +156,39 @@ class TaskListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 3.0,
-      child: ListTile(
-        title: Text(
-          task.title,
-          style: task.isComplete
-              ? TextStyle(decoration: TextDecoration.lineThrough)
-              : TextStyle(),
-        ),
-        trailing: Checkbox(
-          value: task.isComplete,
-          activeColor: primaryColor,
-          onChanged: (value) {
-            Provider.of<Tasks>(context, listen: false)
-                .updateToDo(task: task, complete: value);
-          },
+    return Slidable(
+      actionPane: SlidableBehindActionPane(),
+      actionExtentRatio: 0.25,
+      child: Card(
+        elevation: 3.0,
+        child: Container(
+          color: Colors.white,
+          child: ListTile(
+            title: Text(
+              task.title,
+              style: task.isComplete
+                  ? TextStyle(decoration: TextDecoration.lineThrough)
+                  : TextStyle(),
+            ),
+            trailing: Checkbox(
+              value: task.isComplete,
+              activeColor: primaryColor,
+              onChanged: (value) {
+                Provider.of<Tasks>(context, listen: false)
+                    .updateToDo(task: task, complete: value);
+              },
+            ),
+          ),
         ),
       ),
+      secondaryActions: <Widget>[
+        IconSlideAction(
+          iconWidget: Icon(
+            Icons.delete,
+            color: Colors.red,
+          ),
+        ),
+      ],
     );
   }
 }
